4f572cf99604eabe6e35bb87b1484a180861b240
avisheksvsu <43935863+avisheksvsu@users.noreply.github.com>
2018-11-19 14:15:10 -0500

    Create DateCalc.java

---
+++ DateCalc.java
@@ -0,0 +1,22 @@
+public class DateCalc
+{
+  public int convert(int d)
+  {
+    int y=2018;
+    
+    while (d > 365)
+    {
+      if (y%400==0 || (y%4==0 && y%100 !=0))
+      {
+        d-=366;
+        y++;
+      }
+      else
+      {
+        d-=365;
+        y++;
+      }
+   }
+   return y;
+  }
+}
